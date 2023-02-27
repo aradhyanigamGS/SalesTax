436ff0b41d17be6db167b4ddd19247f4845dd1d7
= <=>
2023-02-27 15:45:53 +0530

    Adding Sales Problem with Oops

---
+++ SalesOops/Input.rb
@@ -0,0 +1,20 @@
+class Input
+  
+  def input
+
+    puts "Taking input"
+    
+    @input = []
+
+    loop do 
+      # puts "sdf"
+      @product = gets.chomp
+      if @product.nil? || @product.empty?
+        break
+      end
+      @input.push(@product)
+    end
+    # puts "#{@input}"
+    @input
+  end
+end
